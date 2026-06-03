33c714ff09081b1e8a710b43fd85abeaffebb9e4
UriGalanti <49553172@est.ort.edu.ar>
2026-06-03 10:25:22 -0300

    agregué clases en la sidebar y creé su css

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,15 +1,19 @@
 import AppLogo from "./AppLogo";
 import NavigationItem from "./NavigationItem";
 import { FaHouse } from "react-icons/fa6";
-import { FaUser } from "react-icons/fa"; //los iconos fueron seleccionados con chatgpt 
+import { FaUser } from "react-icons/fa"; 
+import "./Sidebar.css";
+//Clases agregadas con IA
 
 const Sidebar = () =>{
     return (
-        <>
+        <nav className="sidebar-container">
             <AppLogo/>
-            <NavigationItem icon={FaHouse} text="Feed" to="/feed"/>
-            <NavigationItem icon={FaUser} text="Profile" to="/profile/me"/>
-        </>
+            <div className="sidebar-nav-items">
+                <NavigationItem icon={FaHouse} text="Feed" to="/feed"/>
+                <NavigationItem icon={FaUser} text="Profile" to="/profile/me"/>
+            </div>
+        </nav>
     )
 }
 export default Sidebar;
