afbb6219dc76113da3acf3ab5d03eab00f3fa7a1
UriGalanti <49553172@est.ort.edu.ar>
2026-06-10 10:33:34 -0300

    Created the styles for profile posts grid

--- src/components/PostsGrid/PostThumbnail/index.tsx
+++ src/components/PostsGrid/PostThumbnail/index.tsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 import type { Post } from "../../../interfaces/Post";
+import './PostTumbnail.css'
 
 //chatgpt: me explico que tenia que pasar el state post a la postpage y lo agregue
 const PostThumbnail = ({ post }: { post: Post }) => {
     return (
-        <Link to={`/post/${post.id}`} state={{ post }}>
+        <Link to={`/post/${post.id}`} state={{ post }} className="post-thumbnail-link">
             <img
                 src={post.image}
                 alt="Post thumbnail"
+                className="post-thumbnail-img"
             />
         </Link>
     )
--- src/components/PostsGrid/index.tsx
+++ src/components/PostsGrid/index.tsx
@@ -1,13 +1,14 @@
 import type { Post } from "../../interfaces/Post";
 import PostThumbnail from "./PostThumbnail";
+import './PostsGrid.css'
 
 const PostGrid = ({ posts }: { posts: Post[] }) => {
     return (
-        <>
+        <div className="post-grid-container">
             {posts.map(post => (
                 <PostThumbnail key={post.id} post={post} />
             ))}
-        </>
+        </div>
     )
 }
 export default PostGrid;
